ecedc64c3c3d43a673b1cfb007a6d614c753b4ce
sonika123 <sunshinesonika@gmail.com>
2017-10-16 17:29:56 +0545

    tygyu

---
+++ app/src/main/java/com/sonika/nepstra/Navigations/NewArrival.java
@@ -0,0 +1,15 @@
+package com.sonika.nepstra.Navigations;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.sonika.nepstra.R;
+
+public class NewArrival extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_arrival);
+    }
+}

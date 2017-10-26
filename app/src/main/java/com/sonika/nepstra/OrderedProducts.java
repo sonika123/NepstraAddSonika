d14720fbcc6debbea888ab2f287dc4481b17eeec
pratik93 <31034554+pratik93@users.noreply.github.com>
2017-10-26 16:53:24 +0545

    ordered items design

--- app/src/main/java/com/sonika/nepstra/OrderedProducts.java
+++ app/src/main/java/com/sonika/nepstra/OrderedProducts.java
@@ -1,11 +1,26 @@
 package com.sonika.nepstra;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.sonika.nepstra.Navigations.ArtAndCraft;
+import com.sonika.nepstra.Navigations.Jwellery;
+import com.sonika.nepstra.Navigations.Kids;
+import com.sonika.nepstra.Navigations.Mens;
+import com.sonika.nepstra.Navigations.NewArrival;
+import com.sonika.nepstra.Navigations.Sports;
+import com.sonika.nepstra.Navigations.Womens;
 import com.sonika.nepstra.adapters.OrderAdapter;
 import com.sonika.nepstra.helpers.OrderHelper;
 import com.sonika.nepstra.listener.ListViewListener;
@@ -20,24 +35,108 @@ import java.util.List;
  * Created by sonika on 9/22/2017.
  */
 
-public class OrderedProducts extends AppCompatActivity implements ListViewListener {
+public class OrderedProducts extends AppCompatActivity implements ListViewListener,NavigationView.OnNavigationItemSelectedListener {
     //RecyclerView orders_recyclerView;
     ListView lv;
     OrderHelper dbhelper;
     OrderAdapter mOrderAdapter;
     List<OrderedProducts_pojo> orderedProductsList = new ArrayList<>();
     TextView totalAmount;
+    DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.ordered_products);
+        setContentView(R.layout.activity_main_for_add_to_cart_design);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setCustomView(R.layout.center_action_bar_text);
+
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setItemIconTintList(null);
+        navigationView.setNavigationItemSelectedListener(this);
+
         dbhelper = new OrderHelper(this);
         lv = (ListView) findViewById(R.id.ordered_productlist);
         totalAmount = (TextView) findViewById(R.id.totalamount);
         getMyTotal();
         show();
     }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_new_arrival) {
+            Intent i = new Intent(this, NewArrival.class);
+            startActivity(i);
+        } else if (id == R.id.nav_mens) {
+            Intent i = new Intent(this, Mens.class);
+            startActivity(i);
+
+        } else if (id == R.id.nav_womens)
+        {
+            Intent i = new Intent(this, Womens.class);
+            startActivity(i);
+
+        } else if (id == R.id.nav_contact) {
+
+        } else if (id == R.id.nav_category) {
+
+        } else if (id == R.id.nav_arts_and_craft) {
+
+            Intent i = new Intent(this, ArtAndCraft.class);
+            startActivity(i);
+
+        }
+
+
+        else if (id == R.id.nav_about_us) {
+            Intent intentAboutUs = new Intent(this, LoginActivity.class);
+            startActivity(intentAboutUs);
+
+        }
+        else if (id == R.id.nav_books) {
+
+            Intent i = new Intent(this, Kids.class);
+            startActivity(i);
+
+        }
+        else if (id == R.id.nav_jewellry) {
+            Intent i = new Intent(this, Jwellery.class);
+            startActivity(i);
+        }
+        else if (id == R.id.nav_sports) {
+            Intent i = new Intent(this, Sports.class);
+            startActivity(i);
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
 
 
     public void show() {
